e4ad50f35a3218a841cfd56bad3d4f5a935ea42a
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-06 16:48:18 -0300

    Resolvendo desafio 6 - área triangulo, retângulo

--- src/challenges.js
+++ src/challenges.js
@@ -31,6 +31,19 @@ const highestCount = arrayNumbers => {
 }
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
+const calcTriangleArea = (base, height) => (base * height) / 2;
+
+const calcRectangleArea = (base, height) => (base * height);
+
+const calcAllAreas = (base, height, form) => {
+  if ( form === 'triângulo') {
+    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}.`;
+  } else if ( form === 'retângulo') {
+    return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}.`;
+  } else {
+    return `Não foi possível fazer o cálculo, insira uma forma geométrica válida.`
+  }
+};
 
 // Desafio 7 - Crie a função catAndMouse
 
