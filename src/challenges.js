57d7cc1a26b33c1481000c108cf9e86a6c36714b
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-06 21:30:47 -0300

    Acertando mini detalhes (retirando '.' e alterando string array vazio para []) nos requisitos 6 e 10 para passar nos testes

--- src/challenges.js
+++ src/challenges.js
@@ -37,11 +37,11 @@ const calcRectangleArea = (base, height) => (base * height);
 
 const calcAllAreas = (base, height, form) => {
   if (form === 'triângulo') {
-    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}.`;
+    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}`;
   } else if (form === 'retângulo') {
-    return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}.`;
+    return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}`;
   } else {
-    return `Não foi possível fazer o cálculo, insira uma forma geométrica válida.`
+    return `Não foi possível fazer o cálculo, insira uma forma geométrica válida`
   }
 };
 
@@ -138,7 +138,7 @@ const techList = (techName, personName) => {
   }
 
   if (techName.length === 0) {
-    return 'O array está vazio!';
+    return [];
   }
 
   return arrayList;
